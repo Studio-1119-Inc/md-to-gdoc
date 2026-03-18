3253490b4f518c23f1f2c6c14350bdd1a51fff50
rob mathews <rob.mathews@goodmeasures.com>
2026-03-18 11:28:19 -0400

    Add release SHA to Homebrew formula

--- Formula/md-to-gdoc.rb
+++ Formula/md-to-gdoc.rb
@@ -2,7 +2,7 @@ class MdToGdoc < Formula
   desc "Convert Markdown files to Google Docs with cross-file link rewriting"
   homepage "https://github.com/Studio-1119-Inc/md-to-gdoc"
   url "https://github.com/Studio-1119-Inc/md-to-gdoc/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "" # Fill after first release: shasum -a 256 v1.0.0.tar.gz
+  sha256 "906ce5eccc0c40e64f0aaf457dbdc91613e51d4246f9b07907e21174a92b565b"
   license "MIT"
 
   depends_on "pandoc"
