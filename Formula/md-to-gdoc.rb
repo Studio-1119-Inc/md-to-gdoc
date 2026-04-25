b6b8490a7a9f47bb9a371140f3a1bd6c8119e620
rob mathews <rob.mathews@goodmeasures.com>
2026-04-25 12:08:49 -0400

    Bump Formula SHA for v1.3.1

--- Formula/md-to-gdoc.rb
+++ Formula/md-to-gdoc.rb
@@ -1,8 +1,8 @@
 class MdToGdoc < Formula
   desc "Convert Markdown files to Google Docs with cross-file link rewriting"
   homepage "https://github.com/Studio-1119-Inc/md-to-gdoc"
-  url "https://github.com/Studio-1119-Inc/md-to-gdoc/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "a9bffd489854e6f54f55ed467946a7106b9cc1e42aca88f6d4c6c9042319d95f"
+  url "https://github.com/Studio-1119-Inc/md-to-gdoc/archive/refs/tags/v1.3.1.tar.gz"
+  sha256 "5b9cef75886a0a6ec7119bc7f6247bdd5a875718c493b9e53aaf892bd0f18f84"
   license "MIT"
 
   depends_on "pandoc"
